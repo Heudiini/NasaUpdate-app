6f16859ed228e3b828fcfe8ddff4a97c25deb51f
Heudiini <77001762+Heudiini@users.noreply.github.com>
2022-05-02 14:19:32 +0100

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [explanation, setExplanation] = useState("");
+  const [day, setDay] = useState("");
+  const [url, setUrl] = useState("");
+  const [title, setTitle] = useState("");
+  const [isReady, setReady] = useState(false);
+  useEffect(() => {
+    fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then((response) => response.json())
+      .then((data) => {
+        setExplanation(data.explanation);
+        setDay(data.date);
+        setUrl(data.url);
+        setTitle(data.title);
+        setReady(true);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  if (!isReady) return <div>Loading...</div>;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img alt="Nasa apod" width="100%" src={url} />
+      <h1>{title}</h1>
+      <h4>{day}</h4>
+      <p>{explanation}</p>
     </div>
   );
 }
